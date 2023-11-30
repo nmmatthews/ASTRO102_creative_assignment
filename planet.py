2b4c3e4af6b30448cf3d410e3e7b479ffd3db93d
nmmatthews <nickmatt@umich.edu>
2023-11-30 11:39:15 -0500

    moved planet class to own file, moving now uses a member function

---
+++ planet.py
@@ -0,0 +1,34 @@
+"""Holds planet class."""
+from turtle import Turtle
+
+X_SHIFT = -700
+
+class Planet():
+    """Holds planet information and turtle object."""
+
+    def __init__(self, color, distance, size, speed):
+        """Set up planet information."""
+        self.color = color
+        self.distance = distance
+        self.size = size
+        self.speed = speed
+        # init turtle object
+        self.turtle = Turtle()
+
+    def set_up_turtle(self):
+        """Set up planet turtle object."""
+        # add turtle attributes
+        self.turtle.color(self.color)
+        self.turtle.shape('circle')
+        self.turtle.speed(1000)
+        # move to correct location
+        self.turtle.penup()
+        self.turtle.goto(self.distance+X_SHIFT, 0)
+        self.turtle.shapesize(self.size)
+        self.turtle.setheading(90)
+        self.turtle.pendown()
+
+    def move_planet(self, delta_d, angle):
+        """Move planet with specified distance and angle"""
+        self.turtle.forward(delta_d)
+        self.turtle.left(angle)
--- solarsystem.py
+++ solarsystem.py
@@ -1,64 +1,39 @@
-from turtle import *
+"""ASTRO 102 CREATIVE ASSIGNMENT: Animated Solar System Relative Distances and Periods."""
 import math
-import time
+from turtle import Turtle
+from turtle import Screen
+from planet import Planet
 
-LEFT_SHIFT = -500
-DISTANCE_MULTIPLIER = 4
-
-
-def create_polygon(turtle):
-    sides = 8
-    angle = 360/sides
-    turtle.setheading(90)    # point due north
-    for sides in range(sides):  # repeat the indented lines 12 times
-        turtle.forward(angle)      # move forward by 40 units
-        turtle.right(angle)        # change ?? to the amount to turn
-
-class Planet():
-    def __init__(self, color, distance, size, speed):
-        self.color = color
-        self.distance = distance
-        self.size = size
-        self.speed = speed
-    
-    def set_up_turtle(self):
-        # init turtle object
-        self.t = Turtle()
-        # add turtle attributes
-        self.t.color(self.color)
-        self.t.shape('circle')
-        self.t.speed(1000)
-        # move to correct location
-        self.t.penup()
-        self.t.goto(self.distance+LEFT_SHIFT, 0)
-        self.t.shapesize(self.size)
-        self.t.setheading(90)
-        self.t.pendown()
+X_SHIFT = -700
+DISTANCE_MULTIPLIER = 5
 
 
 def orbit_planets(planets):
+    """Animate planet orbits with a tick-based system."""
     sides = 90.0
     for _ in range(int(sides)):  # repeat the indented lines 12 times
         # each planet
         for planet in planets:
-            d = 2.0*math.pi*planet.distance/(sides/planet.speed)
-            angle = 360.0/sides
-            planet.t.forward(d)
-            planet.t.left(angle*planet.speed)
+            # this math was harder than it should've been
+            delta_d = 2.0*math.pi*planet.distance/(sides/planet.speed)
+            angle = (360.0/sides)*planet.speed
+            planet.move_planet(delta_d, angle)
 
 
 def set_up_sun():
+    """Create sun turtle and stamp shape."""
     sun = Turtle()
     sun.color('yellow')
     sun.shape('circle')
     sun.shapesize(0.6)
     sun.speed(1000)
     sun.penup()
-    sun.goto(LEFT_SHIFT, 2)
+    sun.goto(X_SHIFT, 2)
     sun.stamp()
 
 
 def create_planets():
+    """Create list of planet objects and set up their turtle object."""
     mercury = Planet('gray30', 3.9*DISTANCE_MULTIPLIER, 0.2, 4.15)
     venus = Planet('OrangeRed2', 7.2*DISTANCE_MULTIPLIER, 0.2, 1.62)
     earth = Planet('green', 10*DISTANCE_MULTIPLIER, 0.2, 1)
@@ -77,18 +52,18 @@ def create_planets():
 
 
 def main():
-    s = Screen()
-    s.bgcolor('black')
+    """Animate Solar System relative distances and periods."""
+    space = Screen()
+    space.bgcolor('black')
 
     set_up_sun()
 
     planets = create_planets()
 
     orbit_planets(planets)
-    
-    s.exitonclick()
+
+    space.exitonclick()
+
 
 if __name__ == '__main__':
     main()
-
-
